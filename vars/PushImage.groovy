91e36dbb0b060d5383bee2862ff5efa0c11da61c
vaibhavbharambe2 <145689323+vaibhavbharambe2@users.noreply.github.com>
2024-12-21 21:06:00 +0530

    Update PushImage.groovy

--- vars/PushImage.groovy
+++ vars/PushImage.groovy
@@ -1,9 +1,8 @@
-def call(){
+def call(String Project, String ImageTag, String dockerhubuser){
   echo "Pushing image to DockerHub"
-                withCredentials([usernamePassword('credentialsId':"DockerHubCredentials",passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")])
-                {
-                    sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
-                    sh "docker image tag notes-app:latest ${env.dockerHubUser}/notes-app:latest"
-                    sh "docker push ${env.dockerHubUser}/notes-app:latest"
-                }
+  withCredentials([usernamePassword('credentialsId':"DockerHubCredentials",passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")])
+  {
+    sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+  }
+  sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
 }
